6abe088b499d7750a546eb6c8da20f7df92328aa
Hp <adkhamjon.abd@gmail.com>
2023-04-18 15:44:25 +0200

    Corrects some logic in ArrayListCreator

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
-        ArrayList<String> divisibleByThree = new ArrayList<String>();
-        for (int i = 0; i < sourceList.size(); i++){
-            if (i % 3 == 0){
-                divisibleByThree.add(sourceList.get(i));
+        ArrayList<String> divisibleByThree = new ArrayList<>();
+        int counter = 1;
+        for (String element : sourceList){
+            if (counter % 3 == 0){
+                divisibleByThree.add(element);
+                divisibleByThree.add(element);
             }
+            counter++;
         }
         return divisibleByThree;
     }
